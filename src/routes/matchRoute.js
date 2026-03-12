25583853682d96d1aef459fc0770e1e6c3a57016
yatishgarg-411 <126184526+yatishgarg-411@users.noreply.github.com>
2026-03-12 15:26:37 +0530

    Bug Resolved

--- src/routes/matchRoute.js
+++ src/routes/matchRoute.js
@@ -1,20 +1,32 @@
 const express = require("express");
 const router = express.Router();
+
 const multer = require("multer");
 
 const matchController = require("../controllers/matchController");
 
 const storage = multer.diskStorage({
- destination: function(req,file,cb){
-   cb(null,"uploads/");
- },
- filename: function(req,file,cb){
-   cb(null, Date.now()+"-"+file.originalname);
- }
+
+  destination: function(req,file,cb){
+
+    cb(null,"uploads/");
+
+  },
+
+  filename: function(req,file,cb){
+
+    cb(null, Date.now()+"-"+file.originalname);
+
+  }
+
 });
 
 const upload = multer({storage});
 
-router.post("/match", upload.single("resume"), matchController.matchResume);
+router.post(
+  "/match",
+  upload.single("resume"),
+  matchController.matchResume
+);
 
 module.exports = router;
