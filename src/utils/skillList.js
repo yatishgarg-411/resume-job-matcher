66a5bd620a21f35a24bbb7eb69fe2a69190a3314
yatishgarg-411 <126184526+yatishgarg-411@users.noreply.github.com>
2026-03-12 15:13:47 +0530

    Skills Dictionary added

---
+++ src/utils/skillList.js
@@ -0,0 +1,28 @@
+const skills = [
+  // Programming Languages
+  "javascript", "typescript", "python", "java", "c", "c++", "c#", "go", "ruby", "php", "swift", "kotlin", "scala", "rust", "perl", "r", "matlab", "objective-c", "dart", "shell", "bash", "powershell",
+  // Web & Frontend
+  "html", "css", "sass", "less", "react", "angular", "vue", "next.js", "nuxt.js", "svelte", "ember.js", "jquery", "bootstrap", "tailwindcss",
+  // Backend & Frameworks
+  "node.js", "node", "express", "nestjs", "django", "flask", "spring", "spring boot", "laravel", "rails", "ruby on rails", "asp.net", "fastapi", "hapi.js", "koa.js", "symfony", "cakephp","fastAPI",
+  // Databases
+  "mysql", "postgresql", "sqlite", "mongodb", "redis", "cassandra", "dynamodb", "oracle", "mariadb", "elasticsearch", "couchdb", "firebase", "neo4j", "influxdb", "snowflake", "bigquery",
+  // Cloud & DevOps
+  "aws", "azure", "gcp", "google cloud", "heroku", "digitalocean", "docker", "kubernetes", "openshift", "jenkins", "travis ci", "circleci", "github actions", "gitlab ci", "terraform", "ansible", "puppet", "chef", "vagrant", "prometheus", "grafana", "new relic", "datadog", "splunk",
+  // Data Science & ML
+  "pandas", "numpy", "scipy", "scikit-learn", "tensorflow", "keras", "pytorch", "matplotlib", "seaborn", "plotly", "jupyter", "hadoop", "spark", "hive", "tableau", "power bi", "mlflow", "nltk", "spacy", "opencv",
+  // Mobile
+  "android", "ios", "react native", "flutter", "xamarin", "cordova", "ionic",
+  // Version Control
+  "git", "svn", "mercurial",
+  // Testing
+  "jest", "mocha", "chai", "jasmine", "pytest", "unittest", "selenium", "cypress", "karma", "enzyme", "junit", "testng",
+  // Security
+  "oauth", "jwt", "ssl", "tls", "saml", "openid connect", "owasp", "burp suite", "wireshark",
+  // Project Management & Tools
+  "jira", "trello", "asana", "slack", "monday.com", "notion", "confluence", "microsoft office", "excel", "word", "powerpoint",
+  // Soft Skills
+  "communication", "leadership", "teamwork", "problem solving", "critical thinking", "adaptability", "time management", "creativity", "collaboration", "attention to detail", "project management"
+];
+
+module.exports = skills;
