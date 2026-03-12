2b9b6c8bc0db6e62ace89636e783ca4d72976729
yatishgarg-411 <126184526+yatishgarg-411@users.noreply.github.com>
2026-03-12 15:22:13 +0530

    Implement skill matching and scoring

--- src/services/matchService.js
+++ src/services/matchService.js
@@ -1,2 +1,28 @@
+function matchSkills(resumeSkills, jdSkills){
 
+  const skillsAnalysis = [];
 
+  jdSkills.forEach(skill => {
+
+    const present = resumeSkills.includes(skill);
+
+    skillsAnalysis.push({
+      skill: skill,
+      presentInResume: present
+    });
+
+  });
+
+  const matchedSkills = skillsAnalysis.filter(
+    skill => skill.presentInResume
+  ).length;
+
+  const score = (matchedSkills / jdSkills.length) * 100;
+
+  return {
+    skillsAnalysis,
+    matchingScore: Math.round(score)
+  };
+}
+
+module.exports = { matchSkills };
